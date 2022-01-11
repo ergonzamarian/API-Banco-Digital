46d3bfd102f9b08cbe3096feb74fd1bf99a62b61
Ergon Zamarian Lima <63436543+ergonlima@users.noreply.github.com>
2022-01-10 22:49:30 -0400

    Add files via upload

---
+++ desafio-rest-api/src/main/java/bancodigital/desafio/rest/api/BancoDigitalApiApplication.java
@@ -0,0 +1,13 @@
+package bancodigital.desafio.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDigitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDigitalApiApplication.class, args);
+	}
+
+}
---
+++ desafio-rest-api/src/test/java/bancodigital/desafio/rest/api/BancoDigitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bancodigital.desafio.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDigitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
